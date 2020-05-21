b78492ae213a642b44ae5c7faf244fb71bd905a4
AlexVelezLl <alanvele@espol.edu.ec>
2020-05-21 09:18:00 -0500

    Gui Pausa

--- src/Vistas/TyperBalloons.java
+++ src/Vistas/TyperBalloons.java
@@ -8,6 +8,7 @@ package Vistas;
 import controlador.Controlador;
 import java.io.File;
 import utilities.CONSTANTES;
+import utilities.Utilities;
 import javafx.application.Application;
 import javafx.scene.Scene;
 import javafx.scene.control.Alert;
@@ -19,63 +20,68 @@ import javafx.scene.media.MediaPlayer;
 import javafx.stage.Stage;
 
 /**
- * Clase principal de la aplicacion desde donde se crea la ventana principal donde
- * sera ejecutado nuestro juego.
+ * Clase principal de la aplicacion desde donde se crea la ventana principal
+ * donde sera ejecutado nuestro juego.
+ * 
  * @author Alex Velez
  */
 public class TyperBalloons extends Application {
     /**
-     * Metodo sobreEscrito a la clase Application donde se desarrollara la 
+     * Metodo sobreEscrito a la clase Application donde se desarrollara la
      * estructura de la aplicacion
+     * 
      * @param primaryStage Stage Principal donde se vera la aplicacion.
      */
     @Override
     public void start(Stage primaryStage) {
-        try{
+        try {
+            Media musicFile = new Media(new File(CONSTANTES.RUTA_SOUNDS + "Game_Intro.mp3").toURI().toString());
+            Controlador.setMediaPlayer(new MediaPlayer(musicFile));
+            Controlador.getMediaPlayer().setAutoPlay(true);
+            Controlador.getMediaPlayer().setVolume(0.7);
+            Controlador.setSondEsp(true);
+            Controlador.setpPausa(Utilities.pPausa());
             VistaInicio index = new VistaInicio();
             Pane backGround = index.getRoot();
             Controlador.setScene(new Scene(backGround));
-            Media musicFile = new Media(new File(CONSTANTES.RUTA_SOUNDS+"Game_Intro.mp3").toURI().toString());
-            Controlador.setMediaPlayer(new MediaPlayer(musicFile));
-            Controlador.getMediaPlayer().setAutoPlay(true);
-  
-            Controlador.getMediaPlayer().setVolume(0.7);
+
             primaryStage.setTitle("TyperBalloons");
             primaryStage.setHeight(CONSTANTES.HEIGHT);
             primaryStage.setWidth(CONSTANTES.WIDTH);
             primaryStage.setResizable(false);
             primaryStage.setScene(Controlador.getScene());
-            primaryStage.getIcons().add(new Image(CONSTANTES.RUTA_IMGS+"ICO_01.png")); 
+            primaryStage.getIcons().add(new Image(CONSTANTES.RUTA_IMGS + "ICO_01.png"));
             primaryStage.show();
-        }catch(IllegalArgumentException ex){
+        } catch (IllegalArgumentException ex) {
             Alert alert = new Alert(AlertType.ERROR);
             alert.setTitle("WOOPS!");
             alert.setContentText("Dificultades tecnicas, por favor revisa que todos los archivos del juego "
-                    + "se encuentren en tu computadora." );
+                    + "se encuentren en tu computadora.");
             alert.showAndWait();
-        }catch(Exception ex){
+        } catch (Exception ex) {
             Alert alert = new Alert(AlertType.ERROR);
             alert.setTitle("WOOPS!");
             alert.setContentText("Dificultades tecnicas, estamos trabajando en ello.\n"
-                    + "Por favor reporta este error en: https://typerballoons.000webhostapp.com\n\n"
-                    + "Error: "+ex.toString());
+                    + "Por favor reporta este error en: https://typerballoons.000webhostapp.com\n\n" + "Error: "
+                    + ex.toString());
             alert.showAndWait();
         }
         
-   
-        
+
     }
-     @Override
-         public void stop(){
-            VistaJuego1.finalizarJuego1();
-        }
+
+    @Override
+    public void stop() {
+        VistaJuego1.finalizarJuego1();
+    }
 
     /**
      * Metodo principal de la aplicacion
-     * @param args 
+     * 
+     * @param args
      */
     public static void main(String[] args) {
         launch(args);
     }
-    
+
 }
--- src/controlador/Controlador.java
+++ src/controlador/Controlador.java
@@ -14,10 +14,11 @@ import utilities.CONSTANTES;
 
 /**
  * Clase mediadora entre varios aspectos tecnicos del juego y el usuario.
+ * 
  * @author Alex Velez
  */
 public final class Controlador {
-    
+
     private static boolean sondEsp;
     private static ImageView bGIndex;
     private static ImageView title;
@@ -25,159 +26,209 @@ public final class Controlador {
     private static int skin;
     private static MediaPlayer mediaPlayer;
     private static Scene sc;
+    private static Pane pPausa;
     /**
-     * Constructor vacio con modificador de acceso private para evitar que 
-     * se puedan crear instancias de esta clase.
+     * Constructor vacio con modificador de acceso private para evitar que se puedan
+     * crear instancias de esta clase.
      */
-    private Controlador(){}
-    
+    private Controlador() {
+    }
+
     /**
-     * Metodo que crea y estructura una interfaz para poder controlar el volumen 
-     * de la aplicacion.
+     * Metodo que crea y estructura una interfaz para poder controlar el volumen de
+     * la aplicacion.
+     * 
      * @return Pane con el diseño del controlador de volumen
      */
-    public static Pane volumenControl(){
-        ImageView barra = new ImageView(new Image(CONSTANTES.RUTA_IMGS+"Bar.png"));
-        ImageView cont = new ImageView(new Image(CONSTANTES.RUTA_IMGS+"BorderBar.png"));
-        ImageView circ = new ImageView(new Image(CONSTANTES.RUTA_IMGS+"Circle.png"));
-        ImageView barraC = new ImageView(new Image(CONSTANTES.RUTA_IMGS+"BrownBar.png"));
-        barraC.setFitWidth(600-mediaPlayer.getVolume()*600);
-        barraC.setLayoutX(592-(1-mediaPlayer.getVolume())*600);
-    
-        Pane pVolumen = new Pane();
+    public static Pane volumenControl() {
         
+        ImageView barra = new ImageView(new Image(CONSTANTES.RUTA_IMGS + "Bar.png"));
+        ImageView cont = new ImageView(new Image(CONSTANTES.RUTA_IMGS + "BorderBar.png"));
+        ImageView circ = new ImageView(new Image(CONSTANTES.RUTA_IMGS + "Circle.png"));
+        ImageView barraC = new ImageView(new Image(CONSTANTES.RUTA_IMGS + "BrownBar.png"));
+        barraC.setFitWidth(600 - mediaPlayer.getVolume() * 600);
+        barraC.setLayoutX(592 - (1 - mediaPlayer.getVolume()) * 600);
+        
+        Pane pVolumen = new Pane();
+
         Pane pcirc = new Pane();
         pcirc.getChildren().add(circ);
-        circ.setLayoutX((mediaPlayer.getVolume()*600)-33);
-        pcirc.setOnMouseDragged(e->{
-            
-            if((e.getSceneX()-133)>=-33 && (e.getSceneX()-133)<=567){
-                barraC.setFitWidth(592-(circ.getLayoutX()+33));
-                barraC.setLayoutX((circ.getLayoutX()+33));
-                circ.setLayoutX(e.getSceneX()-133);
-                mediaPlayer.setVolume((circ.getLayoutX()+33)/600);
+        circ.setLayoutX((mediaPlayer.getVolume() * 600) - 33);
+        pcirc.setOnMouseDragged(e -> {
+
+            if ((e.getSceneX() - 133) >= -33 && (e.getSceneX() - 133) <= 567) {
+                barraC.setFitWidth(592 - (circ.getLayoutX() + 33));
+                barraC.setLayoutX((circ.getLayoutX() + 33));
+                circ.setLayoutX(e.getSceneX() - 133);
+                mediaPlayer.setVolume((circ.getLayoutX() + 33) / 600);
             }
         });
-        pVolumen.getChildren().addAll(barra,barraC,cont,pcirc);
+        pVolumen.getChildren().addAll(barra, barraC, cont, pcirc);
         return pVolumen;
     }
-    
+
+    public static Pane sonidEsp() {
+        ImageView checkBox = new ImageView(new Image(CONSTANTES.RUTA_IMGS + "CheckBox1.png"));
+        ImageView check = new ImageView(new Image(CONSTANTES.RUTA_IMGS + "Check.png"));
+        Pane pCheckBox = new Pane();
+        if (Controlador.isSondEsp()) {
+            pCheckBox.getChildren().addAll(checkBox, check);
+        } else {
+            pCheckBox.getChildren().addAll(checkBox);
+        }
+
+        pCheckBox.setOnMouseClicked(a -> {
+            if (Controlador.isSondEsp()) {
+                Controlador.setSondEsp(false);
+                pCheckBox.getChildren().remove(check);
+            } else {
+                Controlador.setSondEsp(true);
+                pCheckBox.getChildren().add(check);
+            }
+        });
+        return pCheckBox;
+    }
+
     /**
      * Metodo que comprueba si la aplicacion debe tener o no sonidos especiales
+     * 
      * @return valor de verdad de si el usuario desea sonidos especiales
      */
     public static boolean isSondEsp() {
         return sondEsp;
     }
-    
+
     /**
      * Metodo que obtiene el imageView del background principal de la aplicacion
+     * 
      * @return ImageView con el background principal de la aplicacion
      */
     public static ImageView getbGIndex() {
         return bGIndex;
     }
-    
+
     /**
      * Metodo que obtiene el imageView con el titulo de TyperBalloons
+     * 
      * @return Image view con el titulo de TyperBalloons
      */
     public static ImageView getTitle() {
         return title;
     }
-    
+
+    public static Pane getpPausa(){
+        return pPausa;
+    }
+
     /**
      * Metodo que obtiene el imageView del background de la vista donde se ejecuta
      * el nuevo juego.
-     * @return ImageView con el background de la pantalla donde se ejecuta el juego nuevo
+     * 
+     * @return ImageView con el background de la pantalla donde se ejecuta el juego
+     *         nuevo
      */
     public static ImageView getbGGame() {
         return bGGame;
     }
-    
+
     /**
      * Metodo que obtiene que skin se esta ejecutando en la aplicacio
+     * 
      * @return int con el numero identificador de la skin que se esta utilizando
      */
     public static int getSkin() {
         return skin;
     }
-    
+
     /**
-     * Metodo que obtiene el MediaPlayer de la aplicacion donde se esta ejecutando 
+     * Metodo que obtiene el MediaPlayer de la aplicacion donde se esta ejecutando
      * el sonido principal
+     * 
      * @return Media Player de la aplicacion donde se obtiene el sonido de la vista
-     * Principal
+     *         Principal
      */
-    public static MediaPlayer getMediaPlayer(){
+    public static MediaPlayer getMediaPlayer() {
         return mediaPlayer;
     }
-    
+
     /**
      * Obtiene la escena de la aplicacion
+     * 
      * @return Scene con la escena de la aplicacion
      */
-    public static Scene getScene(){
+    public static Scene getScene() {
         return sc;
     }
-    
+
     /**
      * Metodo que modifica el mediaPlayer de la aplicacion
-     * @param mediaPlayer Media Player que desea insertar al MediaPlayer de la 
-     * aplicacion principal
+     * 
+     * @param mediaPlayer Media Player que desea insertar al MediaPlayer de la
+     *                    aplicacion principal
      */
-    public static void setMediaPlayer(MediaPlayer mediaPlayer){
-        Controlador.mediaPlayer=mediaPlayer;
+    public static void setMediaPlayer(MediaPlayer mediaPlayer) {
+        Controlador.mediaPlayer = mediaPlayer;
     }
-    
+
     /**
      * Metodo que modifica la escena de la interfaz Principal
+     * 
      * @param sc Scena con la que se modificara la escena de la interfaz Principal
      */
-    public static void setScene(Scene sc){
-        Controlador.sc=sc;
+    public static void setScene(Scene sc) {
+        Controlador.sc = sc;
     }
-    
+
     /**
-     * Metodo que modifica el  nivel de verdad de los sonidos especiales de la aplicacion
-     * @param sondEsp Valor de verdad para permitir o no los sonidos especiales en 
-     * el juego 
+     * Metodo que modifica el nivel de verdad de los sonidos especiales de la
+     * aplicacion
+     * 
+     * @param sondEsp Valor de verdad para permitir o no los sonidos especiales en
+     *                el juego
      */
     public static void setSondEsp(boolean sondEsp) {
         Controlador.sondEsp = sondEsp;
     }
-    
+
     /**
-     * Metodo que modifica el imageView de la imagen de fondo de la pantalla principal
+     * Metodo que modifica el imageView de la imagen de fondo de la pantalla
+     * principal
+     * 
      * @param bGIndex Image View con el imageView que se desee poner de fondo
      */
     public static void setbGIndex(ImageView bGIndex) {
         Controlador.bGIndex = bGIndex;
     }
-    
+
+    public static void setpPausa(Pane pPausa){
+        Controlador.pPausa = pPausa;
+    }
     /**
      * Metodo con el que se modifica el tituo de TyperBalloons
-     * @param title ImageView con el elemento por el cual se desea cambiar el titulo 
-     * de la aplicacion Principal TyperBalloons
+     * 
+     * @param title ImageView con el elemento por el cual se desea cambiar el titulo
+     *              de la aplicacion Principal TyperBalloons
      */
     public static void setTitle(ImageView title) {
         Controlador.title = title;
     }
-    
+
     /**
      * Metodo que modifica el imageView de la imagen de fondo de la vista de Juego
+     * 
      * @param bGGame ImageView por el cual se cambia el backgroundd de la aplicacion
      */
     public static void setbGGame(ImageView bGGame) {
         Controlador.bGGame = bGGame;
     }
-    
+
     /**
      * Metodo que modifica el numero identificador de la skin que se desea poner.
+     * 
      * @param skin Int con el numero identificador de la skin que se desee elegir.
      */
     public static void setSkin(int skin) {
         Controlador.skin = skin;
     }
-    
+
 }
